e3b28462e735979fa17aac60b5651391fcd0e115
Lee <https://github.com/hehepduk/SpringStart.git>
2020-12-22 18:59:56 +0900

    first commmit

---
+++ src/main/java/hello/helloospring/HellooSpringApplication.java
@@ -0,0 +1,13 @@
+package hello.helloospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellooSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellooSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/helloospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,59 @@
+package hello.helloospring.service;
+
+import hello.helloospring.domain.Member;
+import hello.helloospring.repository.MemberRepository;
+import hello.helloospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalArgumentException e){
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다. 123");
+//        }
+        // then
+    }
+}
